a9cfeca94d2b81da60dc17c04383a88e23c2ea66
RedBoard <ott.thomas68@gmail.com>
2023-02-19 16:29:09 +0100

    (+) map data for storage all user data

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,16 +11,15 @@ function parseJwtToken(token) {
     return JSON.parse(jsonPayload);
 }
 
-
 window.addEventListener('load', async () => {
     const api = new ApiCall();
     const rsp = await new Promise(resolve => {
         chrome.runtime.sendMessage({ command: 'GET_TOKEN' }, resolve);
     });
-    const refreshToken = rsp.refresh_token;
-    const userEmail = parseJwtToken(refreshToken)['login'];
-    api.setUserToken(refreshToken);
+    const userEmail = parseJwtToken(rsp.refresh_token)['login'];
+    api.setUserToken(rsp.refresh_token);
     api.setUserEmail(userEmail);
     console.log(api.getUserToken());
     console.log(api.getUserEmail());
+    console.log(api.getloadData("general_user"));
 });
--- src/scripts/ApiCall.js
+++ src/scripts/ApiCall.js
@@ -1,19 +1,51 @@
 class ApiCall {
+    #loadData;
     constructor() {
         window.localStorage.setItem("refresh_token", null);
         window.localStorage.setItem("user_email", null);
+        this.#loadData = new Map();
+        this.#loadData.set("general_user", this.#getApiData('user/thomas.ott@epitech.eu?format=json'));
+        this.#loadData.set("general_notes", this.#getApiData('user/thomas.ott@epitech.eu/notes?format=json'));
     }
+
     getUserToken() {
         return window.localStorage.getItem("refresh_token");
     }
+
     setUserToken(refreshToken) {
         window.localStorage.setItem("refresh_token", refreshToken);
     }
+
     getUserEmail() {
         return window.localStorage.getItem("user_email");
     }
+
     setUserEmail(userEmail) {
         window.localStorage.setItem("user_email", userEmail);
+    }
+
+    async #getApiData(endpoint) {
+        if (endpoint === undefined || endpoint === null)
+            return null;
+        var config = {
+            method: 'GET',
+            headers: {
+                Cookies: this.getUserToken()
+            }
+        };
+        var request = new Request('https://intra.epitech.eu/' + endpoint, config);
+        return fetch(request, config)
+        .then(function(response) {
+            return response.json();
+        })
+        .catch(function(err) {
+            console.log(err.message);
+            return null;
+        });
+    }
+
+    getloadData(key) {
+        return this.#loadData.get(key);
     }
 }
 
