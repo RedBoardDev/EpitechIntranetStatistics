fb289796dc770950256ea9616000ef14868ae678
OFrank <oscar.frank@epitech.eu>
2023-05-18 16:46:08 +0200

    (+) top bar and switch between catogery is working

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Dashboard from './Dashboard';
+import Roadblocks from './Roadblocks';
+import Hub from './Hub';
+import Timeline from './Timeline';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/roadblocks" element={<Roadblocks />} />
+        <Route path="/hub" element={<Hub />} />
+        <Route path="/timeline" element={<Timeline />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/Hub.js
@@ -0,0 +1,67 @@
+import './Hub.css';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@material-ui/core/Button';
+import { useNavigate } from 'react-router-dom';
+
+function Hub() {
+  const navigate = useNavigate();
+
+  const ButtonSideNav = ({ text, onClick }) => {
+    const handleClick = () => {
+      if (onClick) {
+        onClick();
+      }
+    };
+
+    return (
+      <Button
+        variant="contained"
+        size="large"
+        style={{
+          backgroundColor: 'aliceblue',
+          width: '10%',
+          padding: '15px',
+          margin: '10px 10px',
+          borderRadius: '10px',
+          fontSize: '1.0rem',
+          fontWeight: 'bolder',
+          display: 'flex',
+          alignItems: 'center',
+          textTransform: 'none',
+        }}
+        onClick={handleClick}
+      >
+        {text}
+      </Button>
+    );
+  };
+
+  const Dashboard = () => {
+    navigate('/');
+  };
+
+  const Roadblocks = () => {
+    navigate('/roadblocks'); // Navigate to Roadblocks.js
+  };
+
+  const Hub = () => {
+    navigate('/hub'); // Navigate to Hub.js
+  };
+
+  const Timeline = () => {
+    navigate('/timeline'); // Navigate to Timeline.js
+  };
+
+  return (
+    <div className="SidePanel">
+      <ButtonSideNav text="Dashboard" onClick={Dashboard} />
+      <ButtonSideNav text="Roadblocks" onClick={Roadblocks} />
+      <ButtonSideNav text="Hub" onClick={Hub} />
+      <ButtonSideNav text="Timeline" onClick={Timeline} />
+      HUUUUUUUUUUUUUUUUUUUUB
+    </div>
+  );
+}
+
+export default Hub;
