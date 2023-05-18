a82d7c38b227cbe1e886a89f9043bcb8e0ec8864
OFrank <oscar.frank@epitech.eu>
2023-05-18 23:02:50 +0200

    (/) refont all

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -3,6 +3,7 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 function Dashboard() {
   const navigate = useNavigate();
@@ -15,24 +16,7 @@ function Dashboard() {
     };
 
     return (
-      <Button
-        variant="contained"
-        size="large"
-        style={{
-          backgroundColor: 'aliceblue',
-          color: 'black',
-          width: '10%',
-          padding: '15px',
-          margin: '20px 20px',
-          borderRadius: '10px',
-          fontSize: '1.0rem',
-          fontWeight: 'bolder',
-          display: 'flex',
-          alignItems: 'center',
-          textTransform: 'none',
-        }}
-        onClick={handleClick}
-      >
+      <Button variant="contained" size="large" style={{ backgroundColor: '#03224c', color: 'aliceblue', width: '10%', padding: '15px', margin: '20px 20px', borderRadius: '10px', fontSize: '1.0rem', fontWeight: 'bolder', display: 'flex', alignItems: 'center', textTransform: 'none',}} onClick={handleClick}>
         {text}
       </Button>
     );
@@ -41,23 +25,7 @@ function Dashboard() {
   const GradientBox = ({ text, secondText, colors }) => {
     const gradient = `linear-gradient(to bottom, ${colors.join(', ')})`;
     return (
-      <Box
-        component="span"
-        sx={{
-          p: 2,
-          background: gradient,
-          width: '20%',
-          margin: '10px',
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'center',
-          textAlign: 'center',
-          borderRadius: '10px',
-          fontSize: '1.5rem',
-          fontWeight: 'bolder',
-        }}
-      >
+      <Box component="span" sx={{ p: 2, background: gradient, width: '20%', margin: '10px', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', textAlign: 'center', borderRadius: '10px', fontSize: '1.5rem', fontWeight: 'bolder',}}>
         {text}
         <Box sx={{ fontSize: '1.0rem', fontWeight: 'normal' }}>
           {secondText}
@@ -66,55 +34,93 @@ function Dashboard() {
     );
   };
 
-  const Dashboard = () => {
-    navigate('/');
+  const WriteSideDate = ({ data1, data2, data3 }) => {
+    return (
+      <p style={{ fontSize: '1.2rem', display: 'flex', justifyContent: 'space-between' }}>
+        <span style={{ marginLeft: '3%', marginRight: '1rem' }}>{data1}</span>
+        <span style={{ marginRight: '1rem', fontWeight: 'bold' }}>{data2} {data3}</span>
+      </p>
+    );
   };
 
-  const Roadblocks = () => {
-    navigate('/roadblocks'); // Navigate to Roadblocks.js
+  const [dashboard, setDashboard] = useState(true);
+  const [roadblocks, setRoadblocks] = useState(false);
+  const [hub, setHub] = useState(false);
+  const [timeline, setTimeline] = useState(false);
+
+  const DashboardFunc = () => {
+    setDashboard(true);
+    setRoadblocks(false);
+    setHub(false);
+    setTimeline(false);
   };
 
-  const Hub = () => {
-    navigate('/hub'); // Navigate to Hub.js
+  const RoadblocksFunc = () => {
+    setDashboard(false);
+    setRoadblocks(true);
+    setHub(false);
+    setTimeline(false);
   };
 
-  const Timeline = () => {
-    navigate('/timeline'); // Navigate to Timeline.js
+  const HubFunc = () => {
+    setDashboard(false);
+    setRoadblocks(false);
+    setHub(true);
+    setTimeline(false);
+  };
+
+  const TimelineFunc = () => {
+    setDashboard(false);
+    setRoadblocks(false);
+    setHub(false);
+    setTimeline(true);
   };
 
   var prenom = "bah";
   var nom = "OUAIS";
   var email = "bah.oauis@gmail.com";
-  var city = "Paris";
+  var city = "Mulhouse";
+  var cursus = "Bachelor";
+  var semester = "B4";
   var promotion = "2026";
 
   return (
     <div className='DashBoard'>
-      <div className="TopBar">
-        <ButtonSideNav text="Dashboard" onClick={Dashboard} />
-        <ButtonSideNav text="Roadblocks" onClick={Roadblocks} />
-        <ButtonSideNav text="Hub" onClick={Hub} />
-        <ButtonSideNav text="Timeline" onClick={Timeline} />
-      </div>
-      <div className="BodyPanel">
-        <GradientBox text="zerty" secondText="oui" colors={['#007bff', '#87ceeb']} />
-        <GradientBox text="Hello" secondText="oui" colors={['#007bff', '#87ceeb']} />
-        <GradientBox text="Box 3" secondText="oui" colors={['#007bff', '#87ceeb']} />
-        <GradientBox text="Box 4" secondText="oui" colors={['#007bff', '#87ceeb']} />
-      </div>
-      <div className='MainDiv'>
-        <div className='MainUserInfo'>
-          <img src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar" className="avatar"/>
-          <p>{prenom} {nom}</p>
-          <p>{email}</p>
-          <p>{city}</p>
-          <p>{promotion}</p>
+      <div className='Main' style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <div className='MainUserInfo1'>
+          <img src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar" className="avatar" />
+          <div className='infoUser' style={{ display: 'flex', flexDirection: 'column' }}>
+            <WriteSideDate data1="Name: " data2={nom} data3={prenom}/>
+            <WriteSideDate data1="E-mail: " data2={email} />
+            <WriteSideDate data1="Cursus: " data2={cursus} />
+            <WriteSideDate data1="Semester: " data2={semester} />
+            <WriteSideDate data1="City: " data2={city} />
+            <WriteSideDate data1="Promotion: " data2={promotion} />
+          </div>
         </div>
-        <div className='TimelogBox'>
-          Ca se time le log
+        <div className='MainUserInfo2'>
+          <div className="TopBar1">
+            <ButtonSideNav text="Dashboard" onClick={DashboardFunc} />
+            <ButtonSideNav text="Roadblocks" onClick={RoadblocksFunc} />
+            <ButtonSideNav text="Hub" onClick={HubFunc} />
+            <ButtonSideNav text="Timeline" onClick={TimelineFunc} />
+          </div>
+          <div>
+            {dashboard && <div>
+              dash
+            </div>}
+            {roadblocks && <div>
+              road
+            </div>}
+            {hub && <div>
+              hub
+            </div>}
+            {timeline && <div>
+              time
+            </div>}
+          </div>
         </div>
       </div>
-
     </div>
   );
 }
