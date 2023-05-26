4aa54f8e2b060125201d47ccae7d19772975d085
RedBoard <ott.thomas68@gmail.com>
2023-05-26 10:07:21 +0200

    (+) change barre for timeline and change style

--- src/TimelineComponent.js
+++ src/TimelineComponent.js
@@ -2,6 +2,18 @@ import React from 'react';
 import Timeline, { SidebarHeader, TodayMarker, TimelineHeaders, DateHeader } from 'react-calendar-timeline';
 import 'react-calendar-timeline/lib/Timeline.css';
 
+const CustomTodayMarker = () => {
+  return (
+    <TodayMarker interval={1000}>
+      {({ styles, date }) => (
+        <div className="custom-today-marker" style={styles}>
+          <div className="marker-circle"></div>
+        </div>
+      )}
+    </TodayMarker>
+  );
+};
+
 const TimelineComponent = ({ projects }) => {
   console.log(projects);
   if (!projects) {
@@ -28,7 +40,15 @@ const TimelineComponent = ({ projects }) => {
         className: 'project-item',
         itemProps: {
           'data-tooltip': project.end_register
-        }
+        },
+        itemContentRenderer: ({ item }) => (
+          <div className="project-item-content">
+            <div className="project-title">{item.title}</div>
+            <div className="project-dates">
+              {new Date(item.start_time).toLocaleDateString()} - {new Date(item.end_time).toLocaleDateString()}
+            </div>
+          </div>
+        )
       });
     });
   });
@@ -41,8 +61,8 @@ const TimelineComponent = ({ projects }) => {
       <Timeline
         groups={groups}
         items={items}
-        defaultTimeStart={startDate}
-        defaultTimeEnd={endDate}
+        visibleTimeStart={startDate}
+        visibleTimeEnd={endDate}
         sidebarWidth={300}
         lineHeight={60}
         itemHeightRatio={0.8}
@@ -51,16 +71,17 @@ const TimelineComponent = ({ projects }) => {
         canResize={false}
         canChangeGroup={false}
         stackItems={true}
+        buffer={1}
       >
-        <TimelineHeaders style={{backgroundColor: '#181c25', color:'darkblue'}}>
-          <SidebarHeader canMove={false}/>
+        <TimelineHeaders style={{ backgroundColor: '#181c25', color: 'darkblue' }}>
+          <SidebarHeader canMove={false} />
           <DateHeader
             unit="month"
             labelFormat="MMMM yyyy"
             style={{ pointerEvents: 'none', fontSize: '22px', fontWeight: 'bold' }}
           />
         </TimelineHeaders>
-        <TodayMarker />
+        <CustomTodayMarker />
       </Timeline>
       <style jsx global>{`
         .project-item {
@@ -69,9 +90,21 @@ const TimelineComponent = ({ projects }) => {
           height: 40px;
           display: flex;
           align-items: center;
-          padding: 5 10px;
+          padding: 5px 10px;
           font-size: 20px;
           font-weight: bold;
+        }
+        .project-item-content {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+        }
+        .project-title {
+          margin-bottom: 5px;
+        }
+        .project-dates {
+          font-size: 12px;
+          color: gray;
         }
         .rct-sidebar-header {
           pointer-events: none;
@@ -83,10 +116,22 @@ const TimelineComponent = ({ projects }) => {
           background: none;
           color: black;
         }
-        .rct-today-marker {
-          z-index: 9999;
-          background-color: blue;
-          font-weight: bold;
+        .custom-today-marker {
+          position: absolute;
+          top: 0;
+          left: 0;
+          right: 0;
+          height: 100%;
+        }
+        .marker-circle {
+          position: absolute;
+          top: -10px;
+          left: 50%;
+          transform: translateX(-50%);
+          width: 16px;
+          height: 16px;
+          background-color: white;
+          border-radius: 50%;
         }
       `}</style>
     </div>
