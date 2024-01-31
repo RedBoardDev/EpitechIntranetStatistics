9b7dfffe629cdacfa3970eec60bef1c19408647c
RedBoardDev <ott.thomas68@gmail.com>
2024-01-31 14:31:28 +0100

    [+] App.js structure with Sidebar component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,55 @@
 import React from 'react';
-import './App.css';
+import { Box } from '@mui/material';
+import Sidebar from './components/Sidebar';
+import Dashboard from './components/Dashboard';
 
-const Popup = () => {
+const App = () => {
     return (
-        <div className="popup">
-            <p>Ceci est votres popup React.</p>
-        </div>
+
+        <Box
+            sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100%',
+                width: '100%',
+                bgcolor: '#F0EFF4',
+                color: '#1F364D',
+                borderRadius: 8,
+                border: '4px solid #1F364D',
+                boxShadow: '0px 0px 10px 0px #1F364D',
+                padding: 2,
+                boxSizing: 'border-box',
+            }}
+        >
+            <Box
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    mb: 2,
+                    width: '100%',
+                    height: '100%',
+                }}
+            >
+                <Sidebar />
+                <Dashboard />
+            </Box>
+
+            <Box
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    height: '2%',
+                    width: '100%',
+                }}
+            >
+                Navigationbar
+            </Box>
+        </Box>
     );
 };
 
-export default Popup;
+export default App;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,95 @@
+import React, { useEffect, useState } from 'react';
+import { Box } from '@mui/material';
+
+const Sidebar = () => {
+    const [city, setCity] = useState('-');
+    const [cursus, setCursus] = useState('-');
+    const [email, setEmail] = useState('-');
+    const [name, setName] = useState('-');
+    const [profilPicture, setProfilPicture] = useState('-');
+    const [promo, setPromo] = useState('-');
+    const [semester, setSemester] = useState('-');
+
+    useEffect(() => {
+        const handleSidebarUpdate = (event) => {
+            const { detail } = event;
+            setCity(detail.city ?? '-');
+            setCursus(detail.cursus ?? '-');
+            setEmail(detail.email ?? '-');
+            setName(detail.name ?? '-');
+            setProfilPicture(detail.profilPicture ?? '-');
+            setPromo(detail.promo ?? '-');
+            setSemester(detail.semester ?? '-');
+        };
+
+        window.addEventListener('sidebar', handleSidebarUpdate);
+
+        return () => {
+            window.removeEventListener('sidebar', handleSidebarUpdate);
+        };
+    }, []);
+
+    return (
+        <Box
+            sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'flex-start',
+                alignItems: 'center',
+                width: '22%',
+                height: '100%',
+                borderRadius: 8,
+                border: '2px solid #1F364D',
+                marginRight: 2,
+            }}
+        >
+            <Box
+                sx={{
+                    paddingTop: '30px',
+                    width: '100%',
+                    height: 'auto',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }}
+            >
+                <img
+                    src={profilPicture}
+                    alt="Avatar"
+                    style={{
+                        width: '50%',
+                        height: 'auto',
+                        borderRadius: '20px',
+                        border: '2px solid #1F364D',
+                    }}
+                    onError={(e) => {
+                        e.target.src = 'https://cdn.pixabay.com/photo/2017/02/23/13/05/avatar-2092113_960_720.png';
+                    }}
+                />
+            </Box>
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'left',
+                    width: '94%',
+                    color: 'black',
+                    flex: '1',
+                    paddingLeft: '10%',
+                    justifyContent: 'center',
+                }}
+            >
+                <div style={{ marginBottom: '8px' }}>{name}</div>
+                <div style={{ marginBottom: '8px' }}>{email}</div>
+                <div style={{ marginBottom: '8px' }}>{city}</div>
+                <div style={{ marginBottom: '8px' }}>{cursus}</div>
+                <div style={{ marginBottom: '8px' }}>semester {semester}</div>
+                <div style={{ marginBottom: '8px' }}>{promo}</div>
+            </Box>
+
+        </Box>
+    );
+};
+
+export default Sidebar;
