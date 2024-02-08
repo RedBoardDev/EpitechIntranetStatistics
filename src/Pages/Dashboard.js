aca790557b2e76bacf8851c0e6bafda08ec55580
RedBoardDev <ott.thomas68@gmail.com>
2024-02-08 20:06:10 +0100

    [|] separate timeline content from Dashboard component in TimelineBox component

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,23 +1,11 @@
-import React, { useState } from 'react';
-import { Box, IconButton, Modal, Slide } from '@mui/material';
+import React from 'react';
+import { Box } from '@mui/material';
 import { useData } from '../contexts/DataContext';
 import SummaryCard from '../components/SummaryCard';
-import ChartComponent from "../components/ChartComponent";
-import FullscreenIcon from '@mui/icons-material/Fullscreen';
-import FullscreenExitIcon from '@mui/icons-material/FullscreenExit';
-import LoadingSpinner from '../components/LoadingSpinner';
+import TimelineBox from './TimelineBox';
 
 const Dashboard = () => {
     const { dashboardData, timelineData } = useData();
-    const [isModalOpen, setIsModalOpen] = useState(false);
-
-    const openModal = () => {
-        setIsModalOpen(true);
-    };
-
-    const closeModal = () => {
-        setIsModalOpen(false);
-    };
 
     return (
         <Box
@@ -35,67 +23,7 @@ const Dashboard = () => {
                 { title: "G.P.A", text: dashboardData['GPA'] },
                 { title: "Best TEPitech", text: dashboardData['highestTEpitech'] },
             ]} />
-            <Box
-                sx={{
-                    position: 'relative',
-                    display: 'flex',
-                    flex: '1',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    overflow: 'hidden',
-                    border: '2px solid #1F364D',
-                    borderRadius: '14px',
-                    margin: '18px',
-                    marginTop: '0px',
-                    height: '100%',
-                }}
-            >
-                <LoadingSpinner data={timelineData} />
-
-                {timelineData && timelineData.length > 0 && (
-                    <IconButton
-                        sx={{
-                            position: 'absolute',
-                            top: '4px',
-                            left: '4px',
-                            zIndex: 999
-                        }}
-                        onClick={openModal}
-                    >
-                        <FullscreenIcon />
-                    </IconButton>
-                )}
-                {timelineData && <ChartComponent data={timelineData} />}
-                <Modal
-                    open={isModalOpen}
-                    onClose={closeModal}
-                    style={{
-                        display: 'flex',
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                        zIndex: 99999,
-                    }}
-                >
-                    <Slide direction="up" in={isModalOpen} mountOnEnter unmountOnExit>
-                        <Box sx={{
-                            bgcolor: 'white',
-                            borderRadius: '14px',
-                            boxShadow: 24,
-                            padding: 4,
-                            width: '90%',
-                            height: '90%',
-                        }}>
-                            <IconButton style={{
-                                position: 'absolute',
-                                zIndex: 999,
-                            }} onClick={closeModal}>
-                                <FullscreenExitIcon />
-                            </IconButton>
-                            <ChartComponent data={timelineData} />
-                        </Box>
-                    </Slide>
-                </Modal>
-            </Box>
+            <TimelineBox timelineData={timelineData} />
         </Box>
     );
 };
---
+++ src/Pages/TimelineBox.js
@@ -0,0 +1,86 @@
+import React, { useState } from 'react';
+import { Box, IconButton, Modal, Slide } from '@mui/material';
+import ChartComponent from "../components/ChartComponent";
+import FullscreenIcon from '@mui/icons-material/Fullscreen';
+import FullscreenExitIcon from '@mui/icons-material/FullscreenExit';
+import LoadingSpinner from '../components/LoadingSpinner';
+
+const TimelineBox = ({ timelineData }) => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const openModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
+
+    return (
+        <Box
+            sx={{
+                position: 'relative',
+                display: 'flex',
+                flex: '1',
+                justifyContent: 'center',
+                alignItems: 'center',
+                overflow: 'hidden',
+                border: '2px solid #1F364D',
+                borderRadius: '14px',
+                margin: '18px',
+                marginTop: '0px',
+                height: '100%',
+            }}
+        >
+            <LoadingSpinner data={timelineData} />
+
+            {timelineData && timelineData.length > 0 && (
+                <IconButton
+                    sx={{
+                        position: 'absolute',
+                        top: '2px',
+                        left: '2px',
+                        zIndex: 999
+                    }}
+                    onClick={openModal}
+                >
+                    <FullscreenIcon />
+                </IconButton>
+            )}
+            {timelineData && <ChartComponent data={timelineData} />}
+            <Modal
+                open={isModalOpen}
+                onClose={closeModal}
+                style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    zIndex: 999999,
+                }}
+            >
+                <Slide direction="up" in={isModalOpen} mountOnEnter unmountOnExit>
+                    <Box sx={{
+                        bgcolor: 'white',
+                        borderRadius: '14px',
+                        boxShadow: 24,
+                        padding: 4,
+                        width: '90%',
+                        height: '90%',
+                    }}>
+                        <IconButton style={{
+                            position: 'absolute',
+                            zIndex: 999,
+                            margin: '-18px',
+                        }}
+                            onClick={closeModal}>
+                            <FullscreenExitIcon />
+                        </IconButton>
+                        <ChartComponent data={timelineData} />
+                    </Box>
+                </Slide>
+            </Modal>
+        </Box>
+    );
+};
+
+export default TimelineBox;
