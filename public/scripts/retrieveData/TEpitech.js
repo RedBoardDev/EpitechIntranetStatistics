6ecbecba40d430353a3e5c71ec73dda15e6a05aa
RedBoardDev <ott.thomas68@gmail.com>
2024-04-16 23:40:35 +0200

    feat(get highest TEPitech): improve finder of highest TEPitech of the year using regex and Best tepitech data

--- public/scripts/retrieveData/TEpitech.js
+++ public/scripts/retrieveData/TEpitech.js
@@ -2,9 +2,13 @@ function getHighestTEpitech(dataApi) {
     const generalNotesData = dataApi.getGeneralNotesData();
     var highestTEpitech = 0;
 
+    const regex = /TEPitech(?!.*Self-assessment)/;
     const filteredTEPitech = generalNotesData['notes'].filter(element => {
-        return element.title === "TEPitech" && (element.scolaryear).toString() === dataApi.getScolarYear()
+        return element.codemodule === 'B-ANG-058' && regex.test(element.title) && (element.scolaryear).toString() === dataApi.getScolarYear()
     });
+
+    const bestTEPitechElement = filteredTEPitech.find(element => element.title === "Best TEPitech score of the year");
+    if (bestTEPitechElement) return bestTEPitechElement.final_note;
 
     filteredTEPitech.forEach(element => {
         highestTEpitech = (element.final_note > highestTEpitech ? element.final_note : highestTEpitech)
