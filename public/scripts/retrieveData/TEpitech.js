d00657525112d3f31d5ddb6616afcb497f74f623
RedBoardDev <ott.thomas68@gmail.com>
2024-04-17 01:41:50 +0200

    feat (TEPitech statistics): new tab for tepitech chart with details

--- public/scripts/retrieveData/TEpitech.js
+++ public/scripts/retrieveData/TEpitech.js
@@ -1,11 +1,16 @@
+function getFilteredTEPitech(dataApi, regex, notes) {
+    return notes.filter(element => {
+        // return element.codemodule === 'B-ANG-058' && regex.test(element.title) && (element.scolaryear).toString() === dataApi.getScolarYear()
+        return element.codemodule === 'B-ANG-058' && regex.test(element.title) && (element.scolaryear).toString() === '2021'
+    });
+}
+
 function getHighestTEpitech(dataApi) {
     const generalNotesData = dataApi.getGeneralNotesData();
     var highestTEpitech = 0;
 
     const regex = /TEPitech(?!.*Self-assessment)/;
-    const filteredTEPitech = generalNotesData['notes'].filter(element => {
-        return element.codemodule === 'B-ANG-058' && regex.test(element.title) && (element.scolaryear).toString() === dataApi.getScolarYear()
-    });
+    const filteredTEPitech = getFilteredTEPitech(dataApi, regex, generalNotesData['notes']);
 
     const bestTEPitechElement = filteredTEPitech.find(element => element.title === "Best TEPitech score of the year");
     if (bestTEPitechElement) return bestTEPitechElement.final_note;
@@ -16,4 +21,33 @@ function getHighestTEpitech(dataApi) {
     return highestTEpitech;
 }
 
-export { getHighestTEpitech };
+function extractSectionsAndScores(comment) {
+    const regex = /([\w&]+(?: \w+)*):(\d+)\/(\d+)/g;
+    const sectionsAndScores = {};
+    let match;
+
+    while ((match = regex.exec(comment)) !== null) {
+        const section = match[1].trim().toLowerCase();
+        const score = match[2] ? parseInt(match[2], 10) : null;
+        const total = match[3] ? parseInt(match[3], 10) : null;
+        sectionsAndScores[section] = { score, total };
+    }
+    return sectionsAndScores;
+}
+
+function getTEpitechStatistics(dataApi) {
+    const generalNotesData = dataApi.getGeneralNotesData();
+
+    const regex = /TEPitech/;
+    const filteredTEPitech = getFilteredTEPitech(dataApi, regex, generalNotesData['notes']);
+
+    return filteredTEPitech.map(node => {
+        return {
+            date: node.date ?? null,
+            final_note: node.final_note ?? 0,
+            sections: (extractSectionsAndScores(node.comment) ?? null),
+        };
+    });
+}
+
+export { getHighestTEpitech, getTEpitechStatistics };
--- src/Pages/TEPitech.js
+++ src/Pages/TEPitech.js
@@ -1,7 +1,98 @@
-import React, { useState } from 'react';
+import React from 'react';
+import Chart from 'react-apexcharts';
 import { Box, Skeleton, Typography } from '@mui/material';
+import { useData } from '../contexts/DataContext';
+import './TEPitech.css';
+import { COLORS } from '../styles';
+
+const renderSkeletons = () => {
+    return (
+        <Box sx={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'center', width: '100%', height: '100%' }}>
+            <Box sx={{ height: '100%', width: '100%', margin: '10px' }}>
+                <Skeleton
+                    animation="pulse"
+                    variant="rect"
+                    width="100%"
+                    height="100%"
+                    sx={{ borderRadius: '14px' }}
+                />
+            </Box>
+        </Box>
+    );
+};
+
+const ShowChart = ({ tepitechs }) => {
+
+    if (!tepitechs) return renderSkeletons();
+    const chartData = tepitechs.map((tepitech) => ({
+        x: new Date(tepitech.date).toLocaleDateString(),
+        y: tepitech.final_note,
+    }));
+
+    const chartOptions = {
+        chart: {
+            type: 'line',
+            zoom: {
+                enabled: false,
+            },
+            toolbar: {
+                show: false,
+            },
+            animations: {
+                enabled: true,
+                easing: 'easeinout',
+                speed: 800,
+            },
+        },
+        legend: {
+            show: false
+        },
+        stroke: {
+            curve: 'smooth',
+        },
+        xaxis: {
+            type: 'datetime',
+            categories: chartData.map((data) => data.x),
+            labels: {
+                format: 'dd/MM/yyyy',
+            }
+        },
+        yaxis: {
+            title: {
+                text: 'Notes',
+            },
+            min: 0,
+            max: 980,
+        },
+        tooltip: {
+            custom: ({ series, seriesIndex, dataPointIndex, w }) => {
+                const data = tepitechs[dataPointIndex];
+                const note = data?.final_note || '-';
+                const sections = data?.sections ? Object.entries(data.sections)
+                    .map(([sectionName, { score, total }]) => `<div class="apexcharts-tooltip-section">${sectionName}: ${score}/${total}</div>`)
+                    .join('') : '';
+
+                return `
+                <div class="apexcharts-tooltip-container">
+                  <div class="apexcharts-tooltip-title">${note}/980 points</div>
+                  <div class="apexcharts-tooltip-content">
+                    ${sections}
+                  </div>
+                </div>
+              `;
+            },
+        },
+    };
+
+    return (
+        <Box sx={{ height: 'calc(100% - 60px)', width: '100%' }}> {/* 60px est la hauteur du titre */}
+            <Chart options={chartOptions} series={[{ name: 'TEPitech', data: chartData.map((data) => data.y) }]} type="line" height='100%' />
+        </Box>
+    );
+};
 
 const TEPitech = () => {
+    const { tepitechs } = useData();
 
     return (
         <Box
@@ -12,13 +103,34 @@ const TEPitech = () => {
                 width: '100%',
                 overflow: 'auto',
                 position: 'relative',
-                justifyContent: 'center',
-                alignItems: 'center',
             }}
         >
-            <Typography variant="h5" fontWeight="bold" color="text.secondary" sx={{ color: '#757575' }}>
-                SOON...
-            </Typography>
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    // justifyContent: 'center',
+                    overflow: 'hidden',
+                    borderRadius: '14px',
+                    margin: '18px',
+                    height: '100%',
+                    position: 'relative',
+                    backgroundColor: COLORS.box2,
+                }}
+            >
+                <Box sx={{ width: '100%', height: '100%', paddingTop: '10px' }}>
+                    <Typography variant="h4" sx={{
+                        fontWeight: 'bold',
+                        fontSize: 'clamp(16px, 1.2vw, 24px)',
+                        marginBottom: '2px',
+                        textAlign: 'center',
+                    }}>
+                        TEPitech - Notes
+                    </Typography>
+                    {tepitechs && tepitechs.length === 0 ? renderSkeletons() : ShowChart({ tepitechs })}
+                </Box>
+            </Box>
         </Box>
     );
 };
